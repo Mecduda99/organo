0c53baa1672b829e484e3cf9615ba880fb2b40b6
Mecduda99 <rm98760@fiap.com.br>
2023-05-29 23:55:48 -0300

    "04 - Primeiro Componente: Finalizado"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import logo from './meme-sapinho.webp';
-// import logo from './logo.svg';
-import './App.css';
+import Banner from './componetes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Alô, mãe? Tô aprendendo React!!
-          {/* Edite <code>src/App.js</code> e salve para recarregar. */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
     </div>
   );
 }
--- src/componetes/Banner/Banner.js
+++ src/componetes/Banner/Banner.js
@@ -1,7 +1,11 @@
+import './Banner.css'
+
 function Banner(){
     // JSX
     return (
-        <img src="/imagens/banner.png" alt="banner principal da pagina organo"/>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal da pagina organo"/>
+        </header>
     )
 }
 
